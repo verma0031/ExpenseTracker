c4ce2d83ae14ef7575d80b57c269b0d0e07fd677
verma0031 <peeyushverma888@gmial.com>
2023-04-22 11:57:42 +0530

    user can now login with password encryption

--- controllers/user.js
+++ controllers/user.js
@@ -37,12 +37,17 @@ exports.login = async (req, res, next) => {
     console.log(password);
     const user  = await User.findAll({ where : { email }})
         if(user.length > 0){
-            if(user[0].password === password){
-                res.status(200).json({success: true, message: "User logged in successfully"});
-            }
-            else{
-            return res.status(400).json({success: false, message: 'Password is incorrect'})
-           }
+            bcrypt.compare(password, user[0].password , (err, result) => {
+                if(err){
+                    res.status(500).json({success: false, message: "Something went wrong"});
+                }
+                if(result == true){
+                    res.status(200).json({success: true, message: "User logged in successfully"});
+                }
+                else{
+                    return res.status(400).json({success: false, message: 'Password is incorrect'})
+                }
+            })
         } else {
             return res.status(404).json({success: false, message: 'User Doesnot exitst'})
         }
